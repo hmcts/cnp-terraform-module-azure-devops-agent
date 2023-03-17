6b5e506bb504ac437307b7338c805b9ad1e82000
Your Name <james.ferrari@version1.com>
2023-03-17 12:31:09 +0000

    amend case on kv sku

--- key-vault.tf
+++ key-vault.tf
@@ -2,7 +2,7 @@ resource "azurerm_key_vault" "key_vault" {
   name                        = var.key_vault_name
   resource_group_name         = var.resource_group_name
   location                    = var.location
-  sku_name                    = "Standard"
+  sku_name                    = "standard"
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   enabled_for_disk_encryption = true
   purge_protection_enabled    = false

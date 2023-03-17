39181fee5443d4cbbb03145e04d8559952fd9781
Your Name <james.ferrari@version1.com>
2023-03-17 11:53:40 +0000

    add ouputs, add sbox peering

--- _versions.tf
+++ _versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "azurerm" {}
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.46.0"
+      source                = "hashicorp/azurerm"
+      version               = ">= 3.46.0"
+      configuration_aliases = [azurerm.current_peering, azurerm.prod_peering, azurerm.nonprod_peering, azurerm.sbox_peering]
     }
   }
 }
--- virtual-network-peering.tf
+++ virtual-network-peering.tf
@@ -22,6 +22,12 @@ data "azurerm_virtual_network" "ukw-hub-nonprodi" {
   provider            = azurerm.nonprod_peering
 }
 
+data "azurerm_virtual_network" "hmcts-hub-sbox-int" {
+  name                = "hmcts-hub-sbox-int"
+  resource_group_name = "hmcts-hub-sbox-int"
+  provider            = azurerm.sbox_peering
+}
+
 data "azurerm_virtual_network" "core-infra-vnet-mgmt" {
   name                = "core-infra-vnet-mgmt"
   resource_group_name = "rg-mgmt"
@@ -34,6 +40,7 @@ locals {
     "ukw-hub-prod-int"     = data.azurerm_virtual_network.ukw-hub-prod-int.id
     "hmcts-hub-nonprodi"   = data.azurerm_virtual_network.hmcts-hub-nonprodi.id
     "ukw-hub-nonprodi"     = data.azurerm_virtual_network.ukw-hub-nonprodi.id
+    "hmcts-hub-sbox-int"   = data.azurerm_virtual_network.hmcts-hub-sbox-int.id
     "core-infra-vnet-mgmt" = data.azurerm_virtual_network.core-infra-vnet-mgmt.id
   }
 }
@@ -80,6 +87,15 @@ resource "azurerm_virtual_network_peering" "ukw_hub_nonprodi_TO_vnet" {
   virtual_network_name      = data.azurerm_virtual_network.ukw-hub-nonprodi.name
   remote_virtual_network_id = azurerm_virtual_network.vnet.id
   provider                  = azurerm.nonprod_peering
+  allow_forwarded_traffic   = true
+}
+
+resource "azurerm_virtual_network_peering" "hmcts-hub-sbox-int_TO_vnet" {
+  name                      = "hmcts-hub-sbox-int-TO-${var.vnet_name}"
+  resource_group_name       = data.azurerm_virtual_network.hmcts-hub-sbox-int.resource_group_name
+  virtual_network_name      = data.azurerm_virtual_network.hmcts-hub-sbox-int.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet.id
+  provider                  = azurerm.sbox_peering
   allow_forwarded_traffic   = true
 }
 

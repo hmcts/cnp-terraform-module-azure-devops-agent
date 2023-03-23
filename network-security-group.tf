ee2548ca0fde33ccacf669390833273f5600e5f4
Your Name <james.ferrari@version1.com>
2023-03-23 12:58:56 +0000

    add nsg, subnet association

--- network-security-group.tf
+++ network-security-group.tf
@@ -4,3 +4,8 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = var.resource_group_name
   tags                = var.tags
 }
+
+resource "azurerm_subnet_network_security_group_association" "association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}

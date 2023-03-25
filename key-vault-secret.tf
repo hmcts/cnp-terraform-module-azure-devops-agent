2971131aed8ce33a474e79f007e345fa19fab06e
Your Name <james.ferrari@version1.com>
2023-03-25 09:18:43 +0000

    add dependsOn

--- key-vault-secret.tf
+++ key-vault-secret.tf
@@ -3,6 +3,10 @@ resource "azurerm_key_vault_secret" "vmss-ssh-private-key" {
   value        = tls_private_key.vmss.private_key_openssh
   key_vault_id = azurerm_key_vault.key_vault.id
   tags         = var.tags
+
+  depends_on = [
+    azurerm_key_vault_access_policy.access_policy
+  ]
 }
 
 resource "azurerm_key_vault_secret" "vmss-ssh-public-key" {
@@ -10,4 +14,8 @@ resource "azurerm_key_vault_secret" "vmss-ssh-public-key" {
   value        = tls_private_key.vmss.public_key_openssh
   key_vault_id = azurerm_key_vault.key_vault.id
   tags         = var.tags
+
+  depends_on = [
+    azurerm_key_vault_access_policy.access_policy
+  ]
 }
